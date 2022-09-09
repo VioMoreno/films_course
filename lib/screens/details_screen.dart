57101ae4465587c3d6bdef49da38e47fe07fd6f0
VioMoreno <viomoreno3@gmail.com>
2022-09-09 10:31:16 -0300

    MovieSlider3

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -6,12 +6,51 @@ class DetailsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie'; 
+
+
+
     return  Scaffold(
-      body: Container(
-      child: const Center(
-        child: Text ('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar ()
+        ],
+      )
+    );
+  }
+}
+
+
+class _CustomAppBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return  SliverAppBar(
+      backgroundColor: Colors.yellowAccent,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          alignment: Alignment.bottomCenter,
+          color: Colors.black38,
+          width: double.infinity,
+          child: const Text(
+            'movie.title',
+            style: TextStyle( fontSize: 16),
+            ),
+
+        ),
+
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+        image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+          ),
       ),
-     ),
     );
   }
 }
